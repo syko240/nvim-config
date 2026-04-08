5db1249dc17911291c54b34e876fc316bf405b20
syko240 <syko240@users.noreply.github.com>
2026-04-08 13:00:54 +0200

    Load neo-tree eagerly to fix netrw hijack on nvim .

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,26 +11,10 @@ return {
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
-  cmd = 'Neotree',
+  lazy = false,
   keys = {
     { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
   },
-  init = function()
-    vim.api.nvim_create_autocmd('BufEnter', {
-      group = vim.api.nvim_create_augroup('Neotree_start_directory', { clear = true }),
-      desc = 'Start Neo-tree with directory',
-      once = true,
-      callback = function()
-        if package.loaded['neo-tree'] then
-          return
-        end
-        local stats = vim.uv.fs_stat(vim.api.nvim_buf_get_name(0))
-        if stats and stats.type == 'directory' then
-          require('lazy').load({ plugins = { 'neo-tree.nvim' } })
-        end
-      end,
-    })
-  end,
   opts = {
     filesystem = {
       hijack_netrw_behavior = 'open_default',
